67ec31bffe2f9612d459419f8b7bc1d23abbfc49
tejas.pixel6 <amarthonge6767@gmail.com>
2024-09-27 18:28:43 +0530

    animated container

--- _0_basic/lib/main.dart
+++ _0_basic/lib/main.dart
@@ -31,24 +31,16 @@ class _MyAnimationState extends State<MyAnimation> {
           child: Column( 
           mainAxisAlignment: MainAxisAlignment.center,
             children: [ 
-          
               Row ( 
                 mainAxisAlignment: MainAxisAlignment.center,
-          
                 children: [ 
-          
                   Container( 
                     height: isPressed?200:100,
-          
                     width: isPressed ?200:100,
-          
-                    decoration: BoxDecoration( 
-          
+                    decoration:const  BoxDecoration( 
                       color: Colors.red
                     ),
-                    
                   ),
-          
                   AnimatedContainer(
                     curve: Curves.bounceIn,// this how the effect wills show is handded by the curve property
                     duration: const Duration(seconds: 1),
@@ -60,10 +52,8 @@ class _MyAnimationState extends State<MyAnimation> {
                   )
                 ],
               ),
-          
             ],
           ),
-          
         ),
         floatingActionButton: 
               FloatingActionButton(onPressed: (){ 
